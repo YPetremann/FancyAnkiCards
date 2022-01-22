126b87f818e7f5a53353dad8b66055bc7085db46
Yoann Petremann <y.petremann@gmail.com>
2022-01-22 23:23:49 +0100

    remove unecessary code
    Signed-off-by: Yoann Petremann <y.petremann@gmail.com>

--- src/FancyAnkiCards.dev.js
+++ src/FancyAnkiCards.dev.js
@@ -39,14 +39,10 @@ function loadStyle(url, id) {
 
 (async () => {
   try {
-    console.log("Start loading in dev");
     await loadScript("http://127.0.0.1:5500/src/FancyAnkiCards.js", "FAC");
     await loadStyle("http://127.0.0.1:5500/src/FancyAnkiCards.css", "FAC");
-    console.log("End loading in dev");
   } catch {
-    console.log("Start loading in prod");
     await loadScript("https://ypetremann.github.io/FancyAnkiCards/src/FancyAnkiCards.js", "FAC");
     await loadStyle("https://ypetremann.github.io/FancyAnkiCards/src/FancyAnkiCards.css", "FAC");
-    console.log("End loading in prod");
   }
 })();
