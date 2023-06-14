6cc8396f6ee190dcad3042ef94f8e6c489ef560e
YPetremann <YPetremann@users.noreply.github.com>
2023-06-14 06:47:06 +0000

    deploy: a9db2dda97886ea9eef3741ed807f77fcec00cf8

--- src/FancyAnkiCards.js
+++ src/FancyAnkiCards.js
@@ -1,3 +1,46 @@
+function loadScript(url, id) {
+  if (id && document.getElementById(id + "_JS")) return;
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    if (id) {
+      script.id = id + "_JS";
+    }
+    script.src = url;
+    script.onload = () => {
+      console.info(`Succesfully loaded : ${url}`);
+      resolve();
+    };
+    script.onerror = (e) => {
+      console.error(`Error while loading : ${url}`);
+      script.remove();
+      reject(e);
+    };
+    document.head.appendChild(script);
+  });
+}
+function loadStyle(url, id) {
+  if (id && document.getElementById(id + "_CSS")) return;
+  return new Promise((resolve, reject) => {
+    let link = document.createElement("link");
+    if (id) {
+      link.id = id + "_CSS";
+    }
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    link.onload = () => {
+      console.info(`Succesfully loaded : ${url}}`);
+      resolve();
+    };
+    link.onerror = (e) => {
+      console.error(`Error while loading : ${url}}`);
+      link.remove();
+      reject(e);
+    };
+    document.head.appendChild(link);
+  });
+}
+
 function hashCode(s) {
   return s.split("").reduce((a, b) => {
     a = (a << 5) - a + b.charCodeAt(0);
